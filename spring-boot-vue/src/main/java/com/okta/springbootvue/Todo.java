0227a0bc51bd70053505c3835888d6dd2d4b0f37
Romy Maghsoudi <romy_mag@outlook.com>
2020-02-25 09:27:26 -0500

    completed functionality

--- spring-boot-vue/src/main/java/com/okta/springbootvue/Todo.java
+++ spring-boot-vue/src/main/java/com/okta/springbootvue/Todo.java
@@ -5,12 +5,7 @@ import lombok.*;
 import javax.persistence.Id;  
 import javax.persistence.GeneratedValue;  
 import javax.persistence.Entity;  
-
-/**  
- * Defines a data model with 3 properties, autogen ID, string title, and true/false completed
- * Lombok takes care of the getters and setters
- * This class will be mapped to an in-memory H2 database
-**/
+  
 @Entity  
 @Data  
 @NoArgsConstructor  
--- spring-boot-vue/src/main/java/com/okta/springbootvue/TodoRepository.java
+++ spring-boot-vue/src/main/java/com/okta/springbootvue/TodoRepository.java
@@ -3,6 +3,5 @@ package com.okta.springbootvue;
 import org.springframework.data.jpa.repository.JpaRepository;  
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;  
   
-// Auto-generates all needed methods for a REST API
 @RepositoryRestResource  
 interface TodoRepository extends JpaRepository<Todo, Long> {}
